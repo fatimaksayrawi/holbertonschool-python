5cee031d6106adde8263f3b02f2a0126bb68028e
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-02-03 22:05:07 +0200

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module does one class that has one private integer attribute with 2 exc
+eptions."""
+
+
+def print_square(size):
+    """this function is for printing square"""
+    if type(size) is int:
+        if size >= 0:
+            for iter in range(size):
+                for iter2 in range(size):
+                    print("#", end="")
+                print("")
+        else:
+            raise ValueError("size must be >= 0")
+    else:
+        raise TypeError("size must be an integer")
