fb850a8280a0e2b4ef4093162a702c01a5bea145
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-02-05 00:42:26 +0200

    another error

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""this module has function to print fist name and last name"""
+"""
+    3-say_my_name.py
+    This module contains the following say_my_name function
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """this function take 2 string parameter and return last name and first nam
-    e"""
-    if type(first_name) is str:
-        if type(last_name) is str:
-            print("My name is {} {}".format(first_name, last_name))
-        else:
-            raise TypeError("last_name must be a string")
-    else:
-        raise TypeError("first_name must be a string")
-
+    """
+    Print the following: "My name is <first name> <last name>"
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
