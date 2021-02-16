19531e18badfec88eca779bedc2315c75bc74561
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-02-16 18:55:51 +0200

    task 5

--- 0x0C-python-input_output/5-to_json_string.py
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" convert to json object"""
-
-
-import json
+""" json """
 
 
 def to_json_string(my_obj):
-    """ returns the JSON representation of an object
     """
+    json
+    """
+    import json
     return json.dumps(my_obj)
