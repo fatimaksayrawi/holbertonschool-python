a2a266a2e4dc669d4fccbf89d830063d5d105fc7
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-02-16 18:51:19 +0200

    task 3

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-""" write to file """
+"""module write_file"""
 
 
 def write_file(filename="", text=""):
-    """
-    write to file
-    """
-    chara = len(text)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(text)
-    return chara
+    """function write_file"""
+
+    with open(filename, "w+") as f:
+        return f.write(text)
