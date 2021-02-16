ee94b0b9ae98254dd2a9c906d7a39a1f20a0a98d
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-02-16 17:55:58 +0200

    task 3 edited

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""contain function to write txt to a text file"""
+""" write to file """
 
 
 def write_file(filename="", text=""):
-    """ Write a file with given string and returns the numbers of characters
-        writed
     """
-    with open(filename, "w", encoding="utf-8") as writer:
-        return writer.write(text)
+    write to file
+    """
+    chara = len(text)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+    return chara
