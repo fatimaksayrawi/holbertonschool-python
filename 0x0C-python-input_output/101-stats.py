fa33b60c2766c3695a2b6f5bdbe6cfe6494600d4
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-02-17 11:41:50 +0200

    advanced task 2

--- 0x0C-python-input_output/101-stats.py
+++ 0x0C-python-input_output/101-stats.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
-"""module"""
 
+"""
+101-stats Module
+"""
 
-import sys
+if __name__ == '__main__':
 
+    import sys
 
-x = 0
-arg = 0
-dic = {200: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-for line in sys.stdin:
-    x += 1
+    file_size = 0
+    valid_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in valid_codes}
+    counter = 0
+
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(file_size))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
     try:
-        arg += int(line.split(' ')[-1])
-        line.split(' ')[-2]
-        for k, v in dic.items():
-            if int(line.split(' ')[-2]) == k:
-                dic[k] += 1
-        if x % 10 == 0:
-            print('File size: {}'.format(arg))
-            for k in sorted(dic.items()):
-                print('{}: {}'.format(k[0], k[1]))
+        for l in sys.stdin:
+            counter += 1
+            data = l.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                file_size += int(data[-1])
+            except BaseException:
+                pass
+            if counter % 10 == 0:
+                print_stats(stats, file_size)
+        print_stats(stats, file_size)
     except KeyboardInterrupt:
-        print("KeyboardInterrupt")
+        print_stats(stats, file_size)
+        raise
