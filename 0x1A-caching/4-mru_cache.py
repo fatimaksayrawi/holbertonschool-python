deb914d192de84046e66adeabb6a15dd4ce1f58d
Fatima ksayrawi <fatimakssayrawi12346@gmail.com>
2021-03-24 19:07:46 +0200

    new project

--- 0x1A-caching/4-mru_cache.py
+++ 0x1A-caching/4-mru_cache.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python3
-"""mru cache"""
-from base_caching import BaseCaching
+''' MRU Caching: Create a class MRUCache that inherits from BaseCaching
+                 and is a caching system
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """BaseCaching"""
+    ''' An MRU Cache.
+        Inherits all behaviors from BaseCaching except, upon any attempt to
+        add an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the most recently used entry to
+        accommodate for the new one.
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
+
     def __init__(self):
+        ''' Initialize class instance. '''
         super().__init__()
-        self.head, self.tail = 'head', 'tail'
-        self.next, self.prev = {}, {}
-        self.connect(self.head, self.tail)
+        self.keys = []
 
     def put(self, key, item):
-        """ Add an item"""
-        if key and item:
-            if key in self.cache_data:
-                self._remove(key)
-            self._add(key, item)
-
-    def _remove(self, key):
-        """ Remove an item"""
-        self.connect(self.prev[key], self.next[key])
-        del self.prev[key], self.next[key], self.cache_data[key]
-
-    def connect(self, x, y):
-        """connect items"""
-        self.next[x], self.prev[y] = y, x
-
-    def _add(self, key, item):
-        """ Add an item"""
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
-            print("DISCARD: {}".format(self.prev[self.tail]))
-            self._remove(self.prev[self.tail])
-        self.cache_data[key] = item
-        self.connect(self.prev[self.tail], key)
-        self.connect(key, self.tail)
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard most recently used entry in cache to accommodate new
+            entry. '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """ Get an item"""
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        if key in self.cache_data:
-            val = self.cache_data[key]
-            self._remove(key)
-            self._add(key, val)
-            return val
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            return self.cache_data[key]
+        return None
